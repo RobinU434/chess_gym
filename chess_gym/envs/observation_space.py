dede41562d32fa9b7b598762d68730e7c7ccd8f8
RobinU434 <robin.uhrich@arcor.de>
2022-11-20 17:17:48 +0100

    implemented set_shape function

--- chess_gym/envs/observation_space.py
+++ chess_gym/envs/observation_space.py
@@ -10,22 +10,24 @@ import torch
 
 
 class ChessSpace(spaces.Space):
-    def __init__(self, board: chess.Board, observation_mode, render_size: int = 512, dtype: Optional[np.float64] = np.float64) -> None:
+    def __init__(   self,
+                    board: chess.Board, 
+                    observation_mode,
+                    render_size: int = 512, 
+                    dtype: Optional[np.float64] = np.float64, 
+                    action_encoding: str="action_wise") -> None:
 
         spaces.Dict()
         super().__init__(None, dtype)
         self.board = board
         self.observation_mode = observation_mode
-
+        self.action_encoding_method = action_encoding
+     
         self.board_space = self._get_board_space(self.observation_mode, render_size)
-        # I will pass the possible actions into the environment.
-        # The possible actions are state dependent 
-        #   -> different amount of actions in each state 
-        #   -> No constant value possible / eventually is the encoding dimension possible
-        self._shape = self.board_space.shape, None  
+        self.set_shape()
         self.possible_actions, self.action_symbols = self._get_possible_actions()
 
-        self.action_encodings = self.encode_actions(self.possible_actions, method="action_wise")
+        self.action_encodings = self.one_hot_encoding(self.possible_actions)
     @property
     def n_action_symbols(self):
         return len(self.action_symbols)
@@ -34,18 +36,32 @@ class ChessSpace(spaces.Space):
     def n_actions(self):
         """len of all possible actions"""
         return len(self.possible_actions)
-    
+
     def _get_board_space(self, observation_mode, render_size: int = 512) -> spaces.Space:
 
         observation_spaces = {  'rgb_array': spaces.Box(low = 0, high = 255,
                                                 shape = (render_size, render_size, 3),
-                                                dtype = np.uint8),
+                                                dtype = np.float64),
                                 'piece_map': spaces.Box(low = 0, high = 1,
                                                 shape = (8, 8, 13),
-                                                dtype = np.uint)}
+                                                dtype = np.float64)}
 
         return observation_spaces[observation_mode]
     
+    def set_shape(self):
+         # I will pass the possible actions into the environment.
+        # The possible actions are state dependent 
+        #   -> different amount of actions in each state 
+        #   -> No constant value possible / eventually is the encoding dimension possible 
+        #   -> here one uci action. third dimension depends on action_wise or symbol_wise encoding
+        if self.action_encoding_method == "action_wise":
+            # (one action, batch_size, 4032 possible actions)
+            action_shape = (1, 1, 4032)
+        elif self.action_encoding_method == "symbol_wise":
+            # (one action, uci format, 16 possible action symbols)
+            action_shape = (1, 4, 16)
+        self._shape = self.board_space.shape, action_shape
+
     @staticmethod
     def _get_possible_actions()-> Tuple[List, List]:
         """returns all permuations of chess board squares and all used symbols
@@ -69,18 +85,17 @@ class ChessSpace(spaces.Space):
         
         return actions, all_symbols
 
-    def encode_actions(self, actions, encoding: str="one_hot", method: str = "action_wise"):
-        if encoding == "one_hot":
-            return self.one_hot_encoding(actions, method)
-
-    def one_hot_encoding(self, actions, method: str = "action_wise"):
-        if method == "action_wise":
-            return self.action_wise_encoding(actions)
-        elif method == "symbol_wise":
-            return self.symbol_wise_encodings(actions)
-        else:
-            raise NotImplemented(f"only 'action_wise' and symbol_wise' are supported methods. You chose {method}")
-    
+    def one_hot_encoding(self, actions, method_overwrite: str = None):
+        encoding_dict = {   "action_wise": self.action_wise_encoding,
+                            "symbol_wise": self.symbol_wise_encodings }
+        try:
+            if method_overwrite is not None:
+                return encoding_dict[method_overwrite](actions)
+            else:
+                return encoding_dict[self.action_encoding_method](actions)
+        except KeyError:
+            raise NotImplementedError(f"only 'action_wise' and symbol_wise' are supported methods. You chose {method_overwrite}")    
+       
     def actionToIndex(self, action):
         # Find symbol index from all_letters, e.g. "a1a2" = 0
         return self.possible_actions.index(action)
@@ -116,13 +131,11 @@ class ChessSpace(spaces.Space):
         # Turn a line into a <len_action_sequence x 4 x n_action_symbols>,
         encodings = []
         for action in actions:
-            encoded = self.symbol_wise_encoding(action)
             encodings.append(self.symbol_wise_encoding(action))
 
         return torch.stack(encodings)
-        return torch.cat(encodings, out=out_format)
 
-    def sample(self):
+    def sample(self) -> Tuple[torch.Tensor, torch.tensor]:
         """sample from board space and from possible actions 
 
         Returns:
@@ -132,5 +145,5 @@ class ChessSpace(spaces.Space):
         board_sample = self.board_space.sample()
 
         # sample from action space
-        action_sample = random.sample(self.possible_actions, random.randint(1, 24))
+        action_sample = self.one_hot_encoding(random.choices(self.possible_actions, k=random.randint(1, 24)))
         return board_sample, action_sample
